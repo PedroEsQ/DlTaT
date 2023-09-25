3747a242b2aaf5248e8d38729fe2fa6638b71e8b
Sans <pedroe.succar@gmaail.com>
2023-09-25 16:21:12 -0300

    Expo funcionando corretamente

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image 
+    <View style={styles.container}>  
+      <ImageBackground 
         style={styles.logo}
         source={require('./assets/2023-07-30_18.41.55.png')}
-      />
-      <Text>O que você está fazendo aqui? Circulando! Não tem nada aqui! Além do mais, não sei nem o que EU estou fazendo aqui.</Text>
+        resizeMode='cover'
+      >
+        
+        <Text
+          style={{color:'white', flex:1, display:'flex', fontSize:42, zIndex:1}}
+        >O que você está fazendo aqui? Circulando! Não tem nada aqui! Além do mais, não sei nem o que EU estou fazendo aqui.</Text>
+
+        
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
+      
+    
   );
 }
 
